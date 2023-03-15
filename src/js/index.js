c8c4c7056728c0576906595f08d48780769b373a
Igor Batista <igor.batista.br@gmail.com>
2023-03-15 14:09:20 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,33 @@
+const formulario = document.querySelectorAll('.item')
+const submit = document.getElementById('mensagem')
+const errorMessage = document.querySelectorAll('.msg-obrigatorio')
+
+formulario.forEach(function(input, index){
+    input.addEventListener('change', function(){
+        if(input.value !== ""){
+            input.classList.remove('invalido')
+            input.classList.add('valido')
+            errorMessage[index].classList.remove('mostrar')
+        }else{
+            input.classList.remove('valido')
+            input.classList.add('invalido')
+            errorMessage[index].classList.add('mostrar')
+        }
+    })
+})
+
+submit.addEventListener('click', function(click){
+    click.preventDefault()
+
+    formulario.forEach(function(input, index){
+        if(input.value !== ""){
+            input.classList.remove('invalido')
+            input.classList.add('valid')
+            errorMessage[index].classList.remove('mostrar')
+        }else{
+            input.classList.remove('valido')
+            input.classList.add('invalido')
+            errorMessage[index].classList.add('mostrar')
+        }
+    })
+})
